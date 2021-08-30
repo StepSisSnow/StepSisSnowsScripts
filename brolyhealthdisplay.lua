f68c0e9cbf9fcd118501d290fc2214ec031a811a
StepSisSnow <81532905+StepSisSnow@users.noreply.github.com>
2021-08-30 22:16:49 +0200

    Update brolyhealthdisplay.lua

--- brolyhealthdisplay.lua
+++ brolyhealthdisplay.lua
@@ -1,7 +1,3 @@
-if game.PlaceId == 2050207304 then
-while wait() do
-  local x = game.Workspace.Live["Broly BR"].Humanoid.Health
-  game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Visible = true
-  game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = math.round(x)
- end
-end
+local x = game.Workspace.Live["Broly BR"].Humanoid.Health
+game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Visible = true
+game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = math.round(x)
