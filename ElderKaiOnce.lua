9072207464f57434282a4aa9e6512bec96fb5395
StepSisSnow <81532905+StepSisSnow@users.noreply.github.com>
2021-09-04 23:56:05 +0200

    Update ElderKaiOnce.lua

--- ElderKaiOnce.lua
+++ ElderKaiOnce.lua
@@ -1,1 +1,38 @@
-
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+	workspace.FriendlyNPCs["Elder Kai"]
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "Yes"
+	}
+)
+wait(0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
