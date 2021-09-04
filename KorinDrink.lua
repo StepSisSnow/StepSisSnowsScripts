01ceb23017dda0cc811b3eb6ba755059c032b782
StepSisSnow <81532905+StepSisSnow@users.noreply.github.com>
2021-09-04 23:56:33 +0200

    Update KorinDrink.lua

--- KorinDrink.lua
+++ KorinDrink.lua
@@ -1,1 +1,45 @@
-
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+	workspace.FriendlyNPCs["Korin"]
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+wait(0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "DRINK"
+	}
+)
+wait(0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
