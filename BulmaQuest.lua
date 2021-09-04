69b310067b8788b02c77fea8a17734707127dffd
StepSisSnow <81532905+StepSisSnow@users.noreply.github.com>
2021-09-04 23:30:54 +0200

    Update BulmaQuest.lua

--- BulmaQuest.lua
+++ BulmaQuest.lua
@@ -1,1 +1,62 @@
-
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+	workspace.FriendlyNPCs["Bulma"]
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "Yes"
+	}
+)
+wait(0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+wait(0.7)
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+	workspace.FriendlyNPCs["Spaceship"]
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "No"
+	}
+)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
