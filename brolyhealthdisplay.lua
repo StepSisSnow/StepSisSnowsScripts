629c4e68859d3640a65e70a44848c0080fa571bf
StepSisSnow <81532905+StepSisSnow@users.noreply.github.com>
2021-09-05 01:48:35 +0200

    Update brolyhealthdisplay.lua

--- brolyhealthdisplay.lua
+++ brolyhealthdisplay.lua
@@ -1,3 +1,2 @@
-local x = game.Workspace.Live["Broly BR"].Humanoid.Health
 game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Visible = true
-game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.round(x)
+game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor(game.Workspace.Live["Broly BR"].Humanoid.Health)
