69562a44d0978d91ae90d687b65341b39156fa1b
StepSisSnow <81532905+StepSisSnow@users.noreply.github.com>
2021-09-04 23:55:25 +0200

    Update NamekSpaceShip.lua

--- NamekSpaceShip.lua
+++ NamekSpaceShip.lua
@@ -1,3 +1,66 @@
-
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+	workspace.FriendlyNPCs["Quest Giver"]
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "Yes"
+	}
+)
+wait(0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+wait(0.7)
+local ChatStart = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatStart
+ChatStart:FireServer(
+	workspace.FriendlyNPCs["NamekianShip"]
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
+wait (0.5)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "No"
+	}
+)
+local ChatAdvance = game:GetService("Players").LocalPlayer.Backpack.ServerTraits.ChatAdvance
+ChatAdvance:FireServer(
+	{
+		[1] = "k"
+	}
+)
 game.workspace.FriendlyNPCs["Quest Giver"]:Destroy()
 game.workspace.FriendlyNPCs["Quest Giver"]:Destroy()
+wait(3)
+game:GetService("TeleportService"):Teleport(536102540)
