7854826577d3592b93b5e3e34780df20cddd930c
StepSisSnow <81532905+StepSisSnow@users.noreply.github.com>
2021-09-24 20:11:40 +0200

    Update brolyhealthdisplay.lua

--- brolyhealthdisplay.lua
+++ brolyhealthdisplay.lua
@@ -2,8 +2,8 @@ _G.HealthModeG = ShowHealth
 
 if _G.HealthModeG == "Percent" or _G.HealthModeG == "percent" then
         game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Visible = true
-        game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Atomic's Health: " ..math.floor((game:GetService("Workspace").Live.AtomicWarp.Humanoid.Health / game:GetService("Workspace").Live.AtomicWarp.Humanoid.MaxHealth) * 100) .. "%"
+        game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor((game:GetService("Workspace").Live["Broly BR"].Humanoid.Health / game:GetService("Workspace").Live["Broly BR"].Humanoid.MaxHealth) * 100) .. "%"
 elseif _G.HealthModeG == "Numbers" or _G.HealthModeG == "numbers" then
         game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Visible = true
-        game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Atomic's Health: " ..math.floor(game.Workspace.Live["AtomicWarp"].Humanoid.Health)
+        game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor(game.Workspace.Live["Broly BR"].Humanoid.Health)
 end
