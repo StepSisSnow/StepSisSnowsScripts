a3f3e1336cb5c3d1857d4a0a9df6011befa44af2
StepSisSnow <81532905+StepSisSnow@users.noreply.github.com>
2022-01-08 17:21:44 +0100

    Update cosmeticsforab.lua

--- cosmeticsforab.lua
+++ cosmeticsforab.lua
@@ -1,13 +1,1 @@
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.Bottom.SP.Visible = true
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.Bottom.SP.BackgroundColor3 = 0, 0, 0
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.Bottom.SP.BackgroundTransparency = 0.2
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.Bottom.SP.BorderColor3 = 0, 0, 0
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Watching PornHub or XNXX?"
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.Bottom.SP.TextColor3 = 150, 0, 255
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.Bottom.Stats.Visible = true
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.Bottom.Stats.Namae.Val.Text = "Anonymous"
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.Bottom.Stats.Race.Val.Text = "Your mom's toes"
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.Bottom.Stats.StatPoints.Val.Text = "Autobroly"
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.FullSize.Quests.TextLabel.Text = "Made by Unfair"
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.FullSize.Money.Text = "Version 3.2"
-game:GetService("Players").LocalPlayer.PlayerGui.HUD.FullSize.Quests.ImageLabel:Destroy()
+
