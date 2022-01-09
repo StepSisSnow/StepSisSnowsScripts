a5a918098b0dd43ae92f31681119e14379fad2fa
StepSisSnow <81532905+StepSisSnow@users.noreply.github.com>
2022-01-09 19:52:27 +0100

    Update spbarload.lua

--- spbarload.lua
+++ spbarload.lua
@@ -1,18 +1,22 @@
-local plr = game.Players.LocalPlayer
 coroutine.resume(
     coroutine.create(function()
-        Timer = RejoinTime - RejoinTime
-        while wait(1) do
-        Timer = Timer + 1
-        end
+        local plr = game.Players.LocalPlayer
+        coroutine.resume(
+            coroutine.create(function()
+                Timer = RejoinTime - RejoinTime
+                while wait(1) do
+                    Timer = Timer + 1
+                end
+            end)
+        )
+        repeat wait()
+            plr.PlayerGui.HUD.Bottom.SP.Visible = true
+            _G.HealthModeG = ShowHealth
+            if _G.HealthModeG == "Percent" or _G.HealthModeG == "percent" then
+                plr.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor((game.workspace.Live["Broly BR"].Humanoid.Health / game.workspace.Live["Broly BR"].Humanoid.MaxHealth) * 100) .. "% | Your Health : "..math.floor(plr.Character.Humanoid.Health).." | Duration: ".. Timer.." / ".. RejoinTime
+            elseif _G.HealthModeG == "Numbers" or _G.HealthModeG == "numbers" then
+                plr.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor(game.Workspace.Live["Broly BR"].Humanoid.Health).." | Your Health : "..math.floor(plr.Character.Humanoid.Health).." | Duration: ".. Timer.." / ".. RejoinTime
+            end
+        until(Timer >= RejoinTime)
     end)
 )
-repeat wait()
-    plr.PlayerGui.HUD.Bottom.SP.Visible = true
-    _G.HealthModeG = ShowHealth
-    if _G.HealthModeG == "Percent" or _G.HealthModeG == "percent" then
-            plr.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor((game.workspace.Live["Broly BR"].Humanoid.Health / game.workspace.Live["Broly BR"].Humanoid.MaxHealth) * 100) .. "% | Your Health : "..math.floor(plr.Character.Humanoid.Health).." | Duration: ".. Timer.." / ".. RejoinTime
-    elseif _G.HealthModeG == "Numbers" or _G.HealthModeG == "numbers" then
-            plr.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor(game.Workspace.Live["Broly BR"].Humanoid.Health).." | Your Health : "..math.floor(plr.Character.Humanoid.Health).." | Duration: ".. Timer.." / ".. RejoinTime
-    end
-until(Timer >= RejoinTime)
