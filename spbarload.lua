499a3fd62c8355512990de2583f37a785251f6ae
StepSisSnow <81532905+StepSisSnow@users.noreply.github.com>
2022-01-09 19:46:52 +0100

    Update spbarload.lua

--- spbarload.lua
+++ spbarload.lua
@@ -1,9 +1,18 @@
 local plr = game.Players.LocalPlayer
-_G.HealthModeG = ShowHealth
-if _G.HealthModeG == "Percent" or _G.HealthModeG == "percent" then
-        game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Visible = true
-        game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor((game:GetService("Workspace").Live["Broly BR"].Humanoid.Health / game:GetService("Workspace").Live["Broly BR"].Humanoid.MaxHealth) * 100) .. "% | Your Health : "..math.floor(game:GetService("Workspace").Live[plr.Name].Humanoid.Health)
-elseif _G.HealthModeG == "Numbers" or _G.HealthModeG == "numbers" then
-        game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Visible = true
-        game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor(game.Workspace.Live["Broly BR"].Humanoid.Health).." | Your Health : "..math.floor(game:GetService("Workspace").Live[plr.Name].Humanoid.Health)
-end
+coroutine.resume(
+    coroutine.create(function()
+        Timer = RejoinTime - RejoinTime
+        while wait(1) do
+        Timer = Timer + 1
+        end
+    end)
+)
+repeat wait()
+    plr.PlayerGui.HUD.Bottom.SP.Visible = true
+    _G.HealthModeG = ShowHealth
+    if _G.HealthModeG == "Percent" or _G.HealthModeG == "percent" then
+            plr.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor((game.workspace.Live["Broly BR"].Humanoid.Health / game.workspace.Live["Broly BR"].Humanoid.MaxHealth) * 100) .. "% | Your Health : "..math.floor(plr.Character.Humanoid.Health).." | Duration: ".. Timer.." / ".. RejoinTime
+    elseif _G.HealthModeG == "Numbers" or _G.HealthModeG == "numbers" then
+            plr.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor(game.Workspace.Live["Broly BR"].Humanoid.Health).." | Your Health : "..math.floor(plr.Character.Humanoid.Health).." | Duration: ".. Timer.." / ".. RejoinTime
+    end
+until(Timer >= RejoinTime)
