9155cc2cea484902167705922304fe42ad1040ad
StepSisSnow <81532905+StepSisSnow@users.noreply.github.com>
2022-01-09 18:37:45 +0100

    Update brolyhealthdisplay.lua

--- brolyhealthdisplay.lua
+++ brolyhealthdisplay.lua
@@ -2,8 +2,8 @@ local plr = game.Players.LocalPlayer.Name
 _G.HealthModeG = ShowHealth
 if _G.HealthModeG == "Percent" or _G.HealthModeG == "percent" then
         game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Visible = true
-        game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor((game:GetService("Workspace").Live["Broly BR"].Humanoid.Health / game:GetService("Workspace").Live["Broly BR"].Humanoid.MaxHealth) * 100) .. "% | Your Health : "..math.floor(game:GetService("Workspace").Live[plr.Name].Humanoid.Health)
+        game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor((game:GetService("Workspace").Live["Broly BR"].Humanoid.Health / game:GetService("Workspace").Live["Broly BR"].Humanoid.MaxHealth) * 100) .. "%"
 elseif _G.HealthModeG == "Numbers" or _G.HealthModeG == "numbers" then
         game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Visible = true
-        game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor(game.Workspace.Live["Broly BR"].Humanoid.Health).." | Your Health : "..math.floor(game:GetService("Workspace").Live[plr.Name].Humanoid.Health)
+        game.Players.LocalPlayer.PlayerGui.HUD.Bottom.SP.Text = "Broly's Health: " ..math.floor(game.Workspace.Live["Broly BR"].Humanoid.Health)..
 end
